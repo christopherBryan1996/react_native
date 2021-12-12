d0f012af9ce34ac3345bd9a7fe1bbaf1001462c5
bryan mancilla <mancillabryan1996@outlook.com>
2021-12-11 23:48:05 -0600

    image, navegationn

--- app/app/components/login/index.js
+++ app/app/components/login/index.js
@@ -1,5 +1,5 @@
 import React,{useState} from "react";
-import {View,Text,StyleSheet,TextInput,TouchableOpacity} from 'react-native'
+import {View,Text,StyleSheet,TextInput,TouchableOpacity,Image} from 'react-native'
 import axios from "axios";
 
 function Login(){
@@ -13,6 +13,8 @@ function Login(){
        try {
            const respuesta=await (await axios.post('http://10.0.2.2:3000/api/auth',login)).data
            console.log(respuesta.jwtoken)
+           setemail('')
+           setpassword('')
        } catch (error) {
             console.log('no')
        }
@@ -21,24 +23,33 @@ function Login(){
     
     return (
         <View style={styles.conteiner}>
-            <Text style={styles.title}>Email:</Text>
-            <TextInput 
-            style={styles.text}
-            value={email}
-            onChangeText={val=>setemail(val)}
-            />
-            <Text style={styles.title}>Password:</Text>
-            <TextInput 
-            secureTextEntry//para esconder el password
-            style={styles.text}
-            value={password}
-            onChangeText={val=>setpassword(val)}
-            />
-            <TouchableOpacity style={styles.btn} 
-            onPress={onpresslogin} 
-            >
-                <Text style={styles.title}>Login</Text>
-            </TouchableOpacity>
+            <View style={styles.subconteier}>
+                <Image 
+                    style={styles.img}
+                    //source={{uri:'https://i.pinimg.com/originals/d9/7b/bb/d97bbb08017ac2309307f0822e63d082.jpg'}}
+                    source={{ uri: "https://cdn.icon-icons.com/icons2/67/PNG/512/user_13230.png" }}
+                />
+            </View>
+            <View style={styles.subconteier}>
+                <Text style={styles.title}>Email:</Text>
+                <TextInput 
+                style={styles.text}
+                value={email}
+                onChangeText={val=>setemail(val)}
+                />
+                <Text style={styles.title}>Password:</Text>
+                <TextInput 
+                secureTextEntry//para esconder el password
+                style={styles.text}
+                value={password}
+                onChangeText={val=>setpassword(val)}
+                />
+                <TouchableOpacity style={styles.btn} 
+                onPress={onpresslogin} 
+                >
+                    <Text style={styles.title}>Login</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
@@ -48,6 +59,11 @@ const styles = StyleSheet.create({
         backgroundColor:'#34495e',
         paddingVertical:20,
         paddingHorizontal:30,
+    },
+    subconteier:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'flex-start'
     },
     title:{
         color: '#fff',
@@ -68,9 +84,14 @@ const styles = StyleSheet.create({
         borderColor:'#fff',
         height:45,
         width:'100%',
-        marginTop:100,
+        marginTop:50,
         justifyContent:"center",
         alignItems:"center"
+    },
+    img:{
+        width: 140, 
+        height: 140,
+        tintColor:'white',
     }
 })
 
