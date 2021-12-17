d7a15a4e942b106fce113f823f9a107165232c81
bryan mancilla <mancillabryan1996@outlook.com>
2021-12-16 18:11:43 -0600

    status user

--- api/router/user.js
+++ api/router/user.js
@@ -3,13 +3,16 @@ const router =  express.Router()
 const {user}= require('../database/db')
 const {v4:uuidv4}= require('uuid')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const verificartoke =require('../muddlewares/auth')
+const{SEED} =require('../config')
 
 router.get('/',verificartoke,(req,res)=>{
     user.findAll()
     .then(users=>res.json(users))
     .catch(error=>res.json(error))
 })
+
 router.post('/',async(req,res)=>{
     const {email,phoneNumber,password,displayName,photoURL} = req.body
     const existencia= await user.findAll({
@@ -34,6 +37,19 @@ router.post('/',async(req,res)=>{
 
     }
 })
-
+router.post('/status',(req,res)=>{
+    const {token} = req.body
+    jwt.verify(token,SEED,(err,decoder)=>{
+        if(err){
+            return res.json({status:false})
+        }
+        if(decoder){
+            return res.json({status:true})
+        }
+        
+        console.log(decoder)
+    })
+    
+})
 
 module.exports= router
